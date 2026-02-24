0d0bb72e78b3fc045eb1e010c530e9db2e3f6604
DomiVEVO <d.schaefer0402@gmail.com>
2026-02-24 21:21:53 +0100

    jetziger Zustand um nach conflicts zu suchen

--- group-jam/scripts/PlayerControll.gd
+++ group-jam/scripts/PlayerControll.gd
@@ -1,11 +1,11 @@
 extends CharacterBody2D
 
 @export var speed = 200.0
+@export var knockback_power: float = 500.0
 
 func _physics_process(_delta):
 	var input_direction = Input.get_vector("left", "right", "forward", "back")
-	
-	var iso_velocity = Vector2(input_direction.x,input_direction.y * 0.5 )
+	var iso_velocity = Vector2(input_direction.x, input_direction.y * 0.5)
 	
 	if iso_velocity.length() > 0:
 		velocity = iso_velocity.normalized() * speed
@@ -13,3 +13,14 @@ func _physics_process(_delta):
 		velocity = velocity.move_toward(Vector2.ZERO, speed)
 		
 	move_and_slide()
+
+func knockback(source_velocity: Vector2):
+	var knockback_direction = (source_velocity - velocity).normalized()
+	var iso_knockback = Vector2(knockback_direction.x, knockback_direction.y * 0.5)
+	
+	velocity = iso_knockback.normalized() * knockback_power
+	move_and_slide()
+	
+
+func canMove():
+	pass
