3c62b59f89b74369897df4bb4215dfc7285456d8
DomiVEVO <d.schaefer0402@gmail.com>
2026-02-25 22:49:11 +0100

    simple player anim und parry

--- group-jam/scripts/PlayerControll.gd
+++ group-jam/scripts/PlayerControll.gd
@@ -1,25 +1,49 @@
 extends CharacterBody2D
 class_name PlayerController
 
+@export var speedVal : float = 200.0
+var speed : float = speedVal
 
-@export var speed : float = 200.0
+#für Parry The Platypus
+var can_move : bool = true:
+	set(value):
+		can_move = value
+		if value == false:
+			speed = 0
+		else:
+			speed = speedVal
+
 @export var knockback_power : float = 500.0
 
-var is_parrying : bool = false 
+@onready var animation = $AnimationPlayer
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("parry"):
+		parry()
 
 func _ready():
 	CompanionLogic.player = self
 	CompanionLogic.container = self.find_child("companion_container")
 
 func _physics_process(_delta):
+	if not can_move:
+		velocity = Vector2.ZERO 
+		move_and_slide()
+		return
+	
 	var input_direction = Input.get_vector("left", "right", "forward", "back")
 	var iso_velocity = Vector2(input_direction.x, input_direction.y * 0.5)
 
 	if iso_velocity.length() > 0:
 		velocity = iso_velocity.normalized() * speed
+		animation.play("running")
 		SignalBus.player_move.emit()
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, speed)
+		animation.play("idle")
+		
+	if velocity.x != 0:
+		$Sprite2D.flip_h = velocity.x < 0
 
 	move_and_slide()
 
@@ -36,9 +60,16 @@ func knockback(source_velocity: Vector2):
 	move_and_slide()
 	
 func parry():
-	is_parrying = true
-	await get_tree().create_timer(0.2).timeout
-	is_parrying = false
+	if not can_move: 
+		return 
+		
+	can_move = false 
+	
+	animation.play("parry")
+	await animation.animation_finished
+	$ParryHitbox/CollisionShape2D.set_deferred("disabled", true)
+	
+	can_move = true 
 
 ## ability use logic
 #ability_type_list contains all types of companions
@@ -65,7 +96,9 @@ func cast_ability():
 
 func cast_explosion():
 	pass
+
 func cast_wall():
 	pass
+
 func cast_teleport():
 	pass
