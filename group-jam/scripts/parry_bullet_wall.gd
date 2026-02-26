b0c41f399e8c240f1021e1c0be9d7376535aab05
DomiVEVO <d.schaefer0402@gmail.com>
2026-02-26 12:30:38 +0100

    kj

--- group-jam/scripts/parry_bullet_wall.gd
+++ group-jam/scripts/parry_bullet_wall.gd
@@ -15,7 +15,7 @@ func _ready() -> void:
 
 func _on_area_entered(area: Area2D) -> void:
 	if area.name == "ParryHitbox":
-		print("parried Bullet")
+		print("parried Bullet wall")
 		isParried()   
 	
 	if area.name == "EffectiveHitboxCompanion":
