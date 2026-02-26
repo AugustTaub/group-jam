4f47d36e0d8a7d1f9d029b506311802c0796a747
Siri667 <cal123@web.de>
2026-02-26 16:35:17 +0100

    cannon_obj_done
    Cannon und 2 Patterns sind fertig

--- group-jam/scripts/bullet.gd
+++ group-jam/scripts/bullet.gd
@@ -1,12 +1,11 @@
 extends Node2D
 
 @export var speed: float = 200.0
-var move_direction = Vector2(-1, 1)
+@export var move_direction = Vector2(0, 0)
 
 func _process(delta: float) -> void:
-	var iso_velocity = Vector2(move_direction.x, move_direction.y * 0.5)
-	self.position += iso_velocity.normalized() * speed * delta
-
+	var iso_velocity = Vector2(move_direction.x, move_direction.y)
+	self.global_position += self.global_position.direction_to( iso_velocity) * speed * delta
 # TO-DO
 # bis jetzt simpler Knockback, der ist aber arsch und 
 # für tests gedacht, guter muss noch implementiert werden
@@ -14,6 +13,4 @@ func _on_body_entered(body: Node2D) -> void:
 	if body.has_method("knockback"):
 		var bullet_velocity = move_direction.normalized() * speed
 		body.knockback(bullet_velocity)
-	
-	print("hit")
 	queue_free()
--- group-jam/scripts/cannon.gd
+++ group-jam/scripts/cannon.gd
@@ -1,22 +1,81 @@
 extends Node2D
 
-@export var id : int
+@export var enabled : bool = true
+var can_shoot : bool = false
+
+@export_enum("down","left","right","up") var direction : String = "down"
+@export var interval : float = 1.0
+@export_enum("single_straight", "three_spray") var pattern : String = "single_straight"
 
 @onready var cannon_anim = find_child("cannon_animation")
 @onready var blow_anim = find_child("blow_animation")
+	
+@onready var pattern_ancor = find_child("pattern_ancor")
+@onready var pattern_position = find_child("pattern_position")
+var pattern_node : Node2D
 
 @onready var bullet = preload("res://scenes/bullet.tscn")
 @onready var parry_bullet = preload("res://scenes/parry_bullet.tscn")
 
+func _ready():
+	instantiate_pattern()
+	apply_direction()
+	blow_anim.play("blow") # important because of selected startframe only first animation doesnt play
+	cannon_anim.play("start_" + direction)
+	await cannon_anim.animation_finished
+	can_shoot = true
+		
+func _process(delta: float) -> void:
+	if enabled == true:
+		if can_shoot == true:
+			can_shoot = false
+			shoot()
+			await get_tree().create_timer(interval).timeout
+			can_shoot = true
+
+#play anim and shoot bullet
 func shoot():
-	cannon_anim.play("shoot")
+	cannon_anim.play("shoot_" + direction)
+	fill_pattern()
 	blow_anim.play("blow")
-	create_bullet(1)
 	await cannon_anim.animation_finished
 
-func create_bullet(bullet_type):
+#instantiate bullet
+func create_bullet(bullet_type : int, target : Vector2):
 	var new_bullet : Node
 	match(bullet_type):
 		1: new_bullet = bullet.instantiate()
 		2: new_bullet = parry_bullet.instantiate()
+	new_bullet.move_direction = target
 	self.add_child(new_bullet)
+
+func fill_pattern():
+	for vector in pattern_node.get_children():
+		var target = vector.get_child(0)
+		var new_target =  target.global_position - self.global_position
+		create_bullet(1,new_target * 1000)
+
+#sets new direction for vectors of bullets based on rotation selected
+func apply_direction():
+	var pattern_rotation : int = 0
+	match(direction):
+		"down": pattern_rotation = 45 + 10
+		"left": pattern_rotation = 135 + 10
+		"right": pattern_rotation = -135 - 10
+		"up": pattern_rotation = 225 + 10
+		
+	pattern_ancor.rotation_degrees = pattern_rotation
+	blow_anim.global_position = pattern_position.global_position
+
+func instantiate_pattern():
+	
+	var pattern_type : PackedScene = null
+	match(pattern):
+		"single_straight": pattern_type = preload("res://scenes/patterns/pattern_single_straight.tscn")
+		"three_spray": pattern_type = preload("res://scenes/patterns/pattern_three_spray.tscn")
+	
+	var new_pattern = pattern_type.instantiate()
+	pattern_position.add_child(new_pattern)
+	pattern_node = new_pattern
+	
+	
