c1d139ffb828e2691c286903fed8cb967eed8832
Siri667 <cal123@web.de>
2026-02-28 11:28:04 +0100

    rng

--- group-jam/scripts/cannon.gd
+++ group-jam/scripts/cannon.gd
@@ -88,9 +88,9 @@ func apply_direction():
 	var pattern_rotation : int = 0
 	match(direction):
 		"down": pattern_rotation = 45 + 10
-		"left": pattern_rotation = 135 + 10
-		"right": pattern_rotation = -135 - 10
-		"up": pattern_rotation = 225 + 10
+		"left": pattern_rotation = 65
+		"right": pattern_rotation = 360 -135 + 10
+		"up": pattern_rotation = 360 - 225 + 39
 		
 	pattern_ancor.rotation_degrees = pattern_rotation
 	blow_anim.global_position = pattern_position.global_position
