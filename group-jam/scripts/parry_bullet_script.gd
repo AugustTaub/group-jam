74e7e26f8351eb4ae2baee88d3b0e19a8b89800a
DomiVEVO <d.schaefer0402@gmail.com>
2026-02-28 21:35:35 +0100

    Zone 3 and Parry Bullet Linger fix
    Zone 3 geadded problem mir der Parry Bullet, das die Hitbox nach Parry noch draußen war wurde gefixt --> ist nach Parry disabled

--- group-jam/scripts/parry_bullet_script.gd
+++ group-jam/scripts/parry_bullet_script.gd
@@ -68,7 +68,8 @@ func isParried():
 	
 	in_freeze = true
 	set_process(false) 
-	set_deferred("monitoring", false)
+	hitbox.get_child(0).set_deferred("disabled", true)
+
 	
 	#TODO aus irgendeinem grund kann es sein das der character 
 	#im freeze state bleibt. ich habe keine ahnung wieso oder warum
