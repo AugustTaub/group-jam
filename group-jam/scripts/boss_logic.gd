1d2c55db592a9eddb3e20385ed316eb7327ca691
Siri667 <cal123@web.de>
2026-03-01 14:11:38 +0100

    some overworks of tiles
    tilemaps tutorial,zone1, edited
    boss minor changes
    tilemap layer changes

--- group-jam/scripts/boss_logic.gd
+++ group-jam/scripts/boss_logic.gd
@@ -3,8 +3,10 @@ extends Node2D
 @onready var boss_anim = find_child("boss_animation")
 @onready var tentacle_animation = find_child("tentacle_animation")
 @onready var hurtbox = find_child("hurtbox")
+@onready var boss_glow = find_child("boss_glow")
 
 func _ready():
+	boss_glow.visible = false
 	hurtbox.area_entered.connect(convert_boss)
 
 func convert_boss(area : Area2D):
@@ -12,6 +14,7 @@ func convert_boss(area : Area2D):
 		boss_anim.play("boss_off")
 		await boss_anim.animation_finished
 		await get_tree().create_timer(2.0).timeout
+		boss_glow.visible = true
 		tentacle_animation.play("boss_hug")
 		boss_anim.play("boss_on")
 		
